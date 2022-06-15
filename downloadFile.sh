a4f9f48d4f1a31b335b1d35742ee14110e695f36
jorbencas <jorbencas@gmail.com>
2022-06-15 22:17:22 +0200

    Test

--- downloadFile.sh
+++ downloadFile.sh
@@ -1,15 +1,17 @@
-#!bin/bash 
-date=`date +"%Y%m%d"`;# 20210127
+#!/bin/bash 
+date='20210127';#`date +"%Y%m%d"`;# 20210127
 url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_$date.ods";
-echo $url;
-file_in_repo=`${echo $url | rev | cut -d/ -f1 | rev}`;
-mkdir -p "$(dirname "$file_in_repo")";
-wget "$url" -O "$file_in_repo" ;
-
-html="./index.html";
-var2="./files/$file_in_repo";
-headerremove=`grep -o './files/*' $1`;
-var3=`echo $headerremove`;
-echo $var2;
-echo $var3;
-sed 's/$var3/$var2/g' "$html";
+file_in_repo=$(echo $url | rev | cut -d/ -f1 | rev);
+dir='./files';
+if [ ! -d $dir ]; then
+    mkdir $dir;
+fi
+if [ -f "$dir/*.ods" ]; then
+    rm -r $dir/*;
+fi
+var2="$dir/$file_in_repo";
+wget "$url" -O "$var2";
+if [ ! -f $var2 ]; then
+    sleep 3;
+fi
+sed "s|<a[^>]* href=\"[^\"]*|<a download href=\"$var2|g" "index.html";
