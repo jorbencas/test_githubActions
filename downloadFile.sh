4e7edeb9f1d702235e7f4c07c2f0828ac71258d6
jorbencas <jorbencas@gmail.com>
2022-06-15 18:22:31 +0200

    Test

--- downloadFile.sh
+++ downloadFile.sh
@@ -1,14 +1,14 @@
 #!bin/bash 
-date=`date +"%Y%m%d"`# 20210127
-url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_$date.ods"
-file_in_repo=`${echo $url | rev | cut -d/ -f1 | rev}`
-mkdir -p "$(dirname "$file_in_repo")"
-wget "$url" -O "$file_in_repo" 
+date=`date +"%Y%m%d"`;# 20210127
+url="https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov/documentos/Informe_Comunicacion_"+ echo $date + ".ods";
+file_in_repo=`${echo $url | rev | cut -d/ -f1 | rev}`;
+mkdir -p "$(dirname "$file_in_repo")";
+wget "$url" -O "$file_in_repo" ;
 
-html="./index.html"
-var2="./files/$file_in_repo"
-headerremove=`grep -o './files/*' $1`
-var3=`echo $headerremove`
-echo $var2
-echo $var3
-sed 's/$var3/$var2/g' "$html"
+html="./index.html";
+var2="./files/$file_in_repo";
+headerremove=`grep -o './files/*' $1`;
+var3=`echo $headerremove`;
+echo $var2;
+echo $var3;
+sed 's/$var3/$var2/g' "$html";
