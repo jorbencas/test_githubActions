4838749ef1c885579d993600cd20c118a9428f86
jorbencas <jorbencas@gmail.com>
2026-03-02 22:04:07 +0100

    test

--- downloadFile.py
+++ downloadFile.py
@@ -64,7 +64,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Tech Dashboard</title>
 </head>
 <body>
