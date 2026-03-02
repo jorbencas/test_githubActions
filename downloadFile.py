cd1f1c8f1fa1f38e2f74a3246e934b774cc8e18d
jorbencas <jorbencas@gmail.com>
2026-03-02 22:08:05 +0100

    fixing assets

--- downloadFile.py
+++ downloadFile.py
@@ -64,7 +64,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Tech Dashboard</title>
 </head>
 <body>
